3f0a83f349f50e43137bf5d6958504312e24b509
abi90 <abisai.ramos@upr.edu>
2016-09-25 22:03:35 -0400

    Test

--- Client/Angular-CoquiGames/web.js
+++ Client/Angular-CoquiGames/web.js
@@ -3,5 +3,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/Angular-CoquiGames"));
+app.get('*', function(req, res) {
+    res.sendfile('./Angular-CoquiGames/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 app.listen(process.env.PORT || 5000);
