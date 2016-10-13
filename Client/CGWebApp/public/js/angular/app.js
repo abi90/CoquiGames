950216aa21700fbbb306ec4bd279f5e1bcf959d3
jesmarieHernandez <jesmarie.hernandez@upr.edu>
2016-10-13 00:00:50 -0400

    Commit Angular Product Controller

--- Client/CGWebApp/public/js/angular/app.js
+++ Client/CGWebApp/public/js/angular/app.js
@@ -21,163 +21,170 @@ app.config(['$httpProvider', '$routeProvider', function ($httpProvider, $routePr
                 }]
             }
         })
-            .when('/404.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/404.html'
-            })
-            .when('/index.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/home.html'
-            })
-            .when('/about.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/about.html'
-            })
-            .when('/cart.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/cart.html',
-                resolve: {
-                    auth: function ($q, authenticationSvc) {
-                        var userInfo = authenticationSvc.getUserInfo();
-                        if (userInfo) {
-                            return $q.when(userInfo);
-                        } else {
-                            return $q.reject({ authenticated: false });
-                        }
+        .when('/404.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/404.html'
+        })
+        .when('/index.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/home.html'
+        })
+        .when('/about.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/about.html'
+        })
+        .when('/cart.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/cart.html',
+            resolve: {
+                auth: function ($q, authenticationSvc) {
+                    var userInfo = authenticationSvc.getUserInfo();
+                    if (userInfo) {
+                        return $q.when(userInfo);
+                    } else {
+                        return $q.reject({ authenticated: false });
                     }
                 }
-            })
-            .when('/category-grid.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/category-grid.html'
-            })
-            .when('/category-list.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/category-list.html'
-            })
-            .when('/compare.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/compare.html'
-            })
-            .when('/contact.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/contact.html'
-            })
-            .when('/login.html', {
-                controller: 'LoginController',
-                templateUrl: 'views/login.html'
-            })
-            .when('/product.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/product.html'
-            })
-            .when('/product-full.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/product-full.html'
-            })
-            .when('/register.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/register.html'
-            })
-            .when('/typography.html', {
-                controller: 'HomeController',
-                templateUrl: 'views/typography.html'
-            })
-            .when('/wishlist', {
-                controller: 'AccountController',
-                templateUrl: 'views/wishlist.html',
-                resolve: {
-                    auth: function ($q, authenticationSvc) {
-                        var userInfo = authenticationSvc.getUserInfo();
-                        if (userInfo) {
-                            return $q.when(userInfo);
-                        } else {
-                            return $q.reject({ authenticated: false });
-                        }
+            }
+        })
+        .when('/category-grid.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/category-grid.html'
+        })
+        .when('/category-list.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/category-list.html'
+        })
+        .when('/compare.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/compare.html'
+        })
+        .when('/contact.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/contact.html'
+        })
+        .when('/login.html', {
+            controller: 'LoginController',
+            templateUrl: 'views/login.html'
+        })
+        .when('/product.html/:productId', {
+            controller: 'ProductController',
+            templateUrl: 'views/product.html',
+            resolve: {
+                productId: ['$route', function($route) {
+                    var params = $route.current.params;
+                    return params.productId = params.productId;
+                }]
+            }
+
+        })
+        .when('/product-full.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/product-full.html'
+        })
+        .when('/register.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/register.html'
+        })
+        .when('/typography.html', {
+            controller: 'HomeController',
+            templateUrl: 'views/typography.html'
+        })
+        .when('/wishlist', {
+            controller: 'AccountController',
+            templateUrl: 'views/wishlist.html',
+            resolve: {
+                auth: function ($q, authenticationSvc) {
+                    var userInfo = authenticationSvc.getUserInfo();
+                    if (userInfo) {
+                        return $q.when(userInfo);
+                    } else {
+                        return $q.reject({ authenticated: false });
                     }
                 }
-            })
-            .when('/search-list', {
-                controller: 'HomeController',
-                templateUrl: 'views/search-list.html'
-            })
-            .when('/search-grid', {
-                controller: 'HomeController',
-                templateUrl: 'views/search-list.html'
-            })
-            .when('/account-info', {
-                controller: 'AccountController',
-                templateUrl: 'views/account-info.html',
-                resolve: {
-                    auth: function ($q, authenticationSvc) {
-                        var userInfo = authenticationSvc.getUserInfo();
-                        if (userInfo) {
-                            return $q.when(userInfo);
-                        } else {
-                            return $q.reject({ authenticated: false });
-                        }
+            }
+        })
+        .when('/search-list', {
+            controller: 'HomeController',
+            templateUrl: 'views/search-list.html'
+        })
+        .when('/search-grid', {
+            controller: 'HomeController',
+            templateUrl: 'views/search-list.html'
+        })
+        .when('/account-info', {
+            controller: 'AccountController',
+            templateUrl: 'views/account-info.html',
+            resolve: {
+                auth: function ($q, authenticationSvc) {
+                    var userInfo = authenticationSvc.getUserInfo();
+                    if (userInfo) {
+                        return $q.when(userInfo);
+                    } else {
+                        return $q.reject({ authenticated: false });
                     }
                 }
-            })
-            .when('/account-orders', {
-                controller: 'AccountController',
-                templateUrl: 'views/account-orders.html',
-                resolve: {
-                    auth: function ($q, authenticationSvc) {
-                        var userInfo = authenticationSvc.getUserInfo();
-                        if (userInfo) {
-                            return $q.when(userInfo);
-                        } else {
-                            return $q.reject({ authenticated: false });
-                        }
+            }
+        })
+        .when('/account-orders', {
+            controller: 'AccountController',
+            templateUrl: 'views/account-orders.html',
+            resolve: {
+                auth: function ($q, authenticationSvc) {
+                    var userInfo = authenticationSvc.getUserInfo();
+                    if (userInfo) {
+                        return $q.when(userInfo);
+                    } else {
+                        return $q.reject({ authenticated: false });
                     }
                 }
-            })
-            .when('/account-payment', {
-                controller: 'AccountController',
-                templateUrl: 'views/account-payment.html',
-                resolve: {
-                    auth: function ($q, authenticationSvc) {
-                        var userInfo = authenticationSvc.getUserInfo();
-                        if (userInfo) {
-                            return $q.when(userInfo);
-                        } else {
-                            return $q.reject({ authenticated: false });
-                        }
+            }
+        })
+        .when('/account-payment', {
+            controller: 'AccountController',
+            templateUrl: 'views/account-payment.html',
+            resolve: {
+                auth: function ($q, authenticationSvc) {
+                    var userInfo = authenticationSvc.getUserInfo();
+                    if (userInfo) {
+                        return $q.when(userInfo);
+                    } else {
+                        return $q.reject({ authenticated: false });
                     }
                 }
-            })
-            .when('/checkout', {
-                controller: 'AccountController',
-                templateUrl: 'views/checkout.html',
-                resolve: {
-                    auth: function ($q, authenticationSvc) {
-                        var userInfo = authenticationSvc.getUserInfo();
-                        if (userInfo) {
-                            return $q.when(userInfo);
-                        } else {
-                            return $q.reject({ authenticated: false });
-                        }
+            }
+        })
+        .when('/checkout', {
+            controller: 'AccountController',
+            templateUrl: 'views/checkout.html',
+            resolve: {
+                auth: function ($q, authenticationSvc) {
+                    var userInfo = authenticationSvc.getUserInfo();
+                    if (userInfo) {
+                        return $q.when(userInfo);
+                    } else {
+                        return $q.reject({ authenticated: false });
                     }
                 }
-            })
-            .when('/account-address', {
-                controller: 'AccountController',
-                templateUrl: 'views/account-address.html',
-                resolve: {
-                    auth: function ($q, authenticationSvc) {
-                        var userInfo = authenticationSvc.getUserInfo();
-                        if (userInfo) {
-                            return $q.when(userInfo);
-                        } else {
-                            return $q.reject({ authenticated: false });
-                        }
+            }
+        })
+        .when('/account-address', {
+            controller: 'AccountController',
+            templateUrl: 'views/account-address.html',
+            resolve: {
+                auth: function ($q, authenticationSvc) {
+                    var userInfo = authenticationSvc.getUserInfo();
+                    if (userInfo) {
+                        return $q.when(userInfo);
+                    } else {
+                        return $q.reject({ authenticated: false });
                     }
                 }
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
+            }
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 
 }]);
 
---
+++ Client/CGWebApp/public/js/angular/controllers/ProductController.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jesmarie on 10-12-16.
+ */
+app.controller('ProductController', ['$scope', '$location', 'storewsapi','productId',function($scope, $location, storewsapi, productId)
+{
+    $scope.productId = productId;
+    storewsapi.getProduct($scope.productId).then(function(response) {
+        $scope.product = response.data;
+    },
+    function (error) {
+        console.log(JSON.stringify(error));
+        $location.path("/");
+    });
+}]);
