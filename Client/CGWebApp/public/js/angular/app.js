12e482d74385fa8797a4ba471770d1be51b61453
abi90 <abisai.ramos@upr.edu>
2016-10-12 20:44:46 -0400

    Commit Angular Login Controller

--- Client/CGWebApp/public/js/angular/app.js
+++ Client/CGWebApp/public/js/angular/app.js
@@ -112,3 +112,15 @@ app.config(['$httpProvider', '$routeProvider', function ($httpProvider, $routePr
 
 }]);
 
+app.run(["$rootScope", "$location", function ($rootScope, $location) {
+
+    $rootScope.$on("$routeChangeSuccess", function (userInfo) {
+        console.log(userInfo);
+    });
+
+    $rootScope.$on("$routeChangeError", function (event, current, previous, eventObj) {
+        if (eventObj.authenticated === false) {
+            $location.path("/login");
+        }
+    });
+}]);
