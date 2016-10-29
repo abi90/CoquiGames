305e9b3ab010f747681156af8eac54e355e81283
HeribertoBourdon <heriberto.bourdon1@upr.edu>
2016-10-29 14:00:08 -0400

    Test query succesful

---
+++ Server/CGWebServiceApi/DBManager.py
@@ -0,0 +1,37 @@
+import os
+import psycopg2
+import urlparse
+import QueryManager as query
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+
+def connection():
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return conn
+
+
+def fetch_products():
+    try:
+        conn = connection()
+        cur = conn.cursor()
+        cur.execute(query.SELECT_PRODUCT_DETAILS)
+
+        columns = [x[0] for x in cur.description]
+        results = []
+        for row in cur.fetchall():
+            results.append(dict(zip(columns, row)))
+        for e in results:
+            print e
+        conn.close()
+        return results
+    except Exception as e:
+        print e
+
+fetch_products()
---
+++ Server/CGWebServiceApi/QueryManager.py
@@ -0,0 +1,5 @@
+
+"""
+Query for fetching all prodcuts
+"""
+SELECT_PRODUCT_DETAILS = """SELECT * FROM product_details"""
