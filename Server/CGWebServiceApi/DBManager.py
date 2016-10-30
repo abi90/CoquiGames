a9d6dee7abd762ff87e3cb59eb6f7a612fd341fc
abi90 <abisai.ramos@upr.edu>
2016-10-29 20:30:06 -0400

    Added Some queries

--- Server/CGWebServiceApi/DBManager.py
+++ Server/CGWebServiceApi/DBManager.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 import urlparse
-import QueryManager as query
+import QueryManager as Query
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
@@ -21,7 +21,7 @@ def fetch_special_products():
     try:
         conn = connection()
         cur = conn.cursor()
-        cur.execute(query.SELECT_SPECIAL_PRODUCTS)
+        cur.execute(Query.SELECT_SPECIAL_PRODUCTS)
 
         columns = [x[0] for x in cur.description]
         results = []
@@ -39,7 +39,7 @@ def fetch_latest_products():
     try:
         conn = connection()
         cur = conn.cursor()
-        cur.execute(query.SELECT_LATEST_PRODUCTS)
+        cur.execute(Query.SELECT_LATEST_PRODUCTS)
 
         columns = [x[0] for x in cur.description]
         results = []
@@ -52,23 +52,123 @@ def fetch_latest_products():
     except Exception as e:
         print e
 
-def fetch_products():
+
+def fetch_platforms():
+    try:
+
+        conn = connection()
+        cur = conn.cursor()
+        cur.execute(Query.SELECT_PLATFORMS)
+
+        columns = [x[0] for x in cur.description]
+        results = []
+
+        for row in cur.fetchall():
+            results.append(dict(zip(columns, row)))
+
+        for r in results:
+            cur.execute(Query.SELECT_PLATFORM_CONSOLES.format(r['platformid']))
+            columns = [x[0] for x in cur.description]
+            r['consoles'] = []
+            for row in cur.fetchall():
+                r['consoles'].append(dict(zip(columns, row)))
+
+        for r in results:
+            cur.execute(Query.SELECT_PLATFORM_ACCESORIES.format(r['platformid']))
+            r['accesories'] = []
+            for row in cur.fetchall():
+                r['accesories'].append(str(row[0]))
+
+        for r in results:
+            cur.execute(Query.SELECT_PLATFORM_GAME_GENRES.format(r['platformid']))
+            r['gamegen'] = []
+            for row in cur.fetchall():
+                r['gamegen'].append(str(row[0]))
+
+        for e in results:
+            print e
+        conn.close()
+        return results
+    except Exception as e:
+        print e
+
+    def fetch_special_products():
+        try:
+            conn = connection()
+            cur = conn.cursor()
+            cur.execute(Query.SELECT_SPECIAL_PRODUCTS)
+
+            columns = [x[0] for x in cur.description]
+            results = []
+            for row in cur.fetchall():
+                results.append(dict(zip(columns, row)))
+            for e in results:
+                print e
+            conn.close()
+            return results
+        except Exception as e:
+            print e
+
+
+def fetch_product(productid):
     try:
         conn = connection()
         cur = conn.cursor()
-        cur.execute(query.SELECT_PRODUCT_DETAILS)
+        cur.execute(Query.SELECT_PRODUCT_DETAILS.format(productid))
+
+        columns = [x[0] for x in cur.description]
+        result = dict(zip(columns, cur.fetchall()[0]))
+        print result
+        conn.close()
+        return result
+    except Exception as e:
+        print e
+
+
+def fetch_platform_latest_products(platformid):
+    try:
+        conn = connection()
+        cur = conn.cursor()
+        cur.execute(Query.SELECT_PLATFORM_LATEST_PRODUCTS.format(platformid))
 
         columns = [x[0] for x in cur.description]
         results = []
         for row in cur.fetchall():
             results.append(dict(zip(columns, row)))
+
         for e in results:
             print e
+
         conn.close()
         return results
     except Exception as e:
         print e
 
+
+def fetch_platform_special_products(platformid):
+    try:
+        conn = connection()
+        cur = conn.cursor()
+        cur.execute(Query.SELECT_PLATFORM_SPECIAL_PRODUCTS.format(platformid))
+
+        columns = [x[0] for x in cur.description]
+        results = []
+        for row in cur.fetchall():
+            results.append(dict(zip(columns, row)))
+
+        for e in results:
+            print e
+
+        conn.close()
+        return results
+    except Exception as e:
+        print e
+
+
 #fetch_products()
 #fetch_latest_products()
-fetch_special_products()
+#fetch_special_products()
+#fetch_platforms()
+#fetch_product(2)
+#fetch_platform_latest_products(1)
+#fetch_platform_special_products(4)
--- Server/CGWebServiceApi/QueryManager.py
+++ Server/CGWebServiceApi/QueryManager.py
@@ -2,7 +2,7 @@
 """
 Query for fetching all prodcuts
 """
-SELECT_PRODUCT_DETAILS = """SELECT * FROM product_details"""
+SELECT_PRODUCT_DETAILS = """SELECT * FROM product_details WHERE pid = {0}"""
 
 SELECT_LATEST_PRODUCTS = """SELECT *
                             FROM product_details
@@ -11,3 +11,29 @@ SELECT_LATEST_PRODUCTS = """SELECT *
 SELECT_SPECIAL_PRODUCTS = """SELECT *
                              FROM product_details
                              WHERE INOFFER = true"""
+SELECT_PLATFORMS = """SELECT pa.platformid, pa.logoimg, pa.platform
+                      FROM platform AS pa
+                      WHERE pa.active = TRUE"""
+
+SELECT_PLATFORM_CONSOLES = """SELECT productid, product_title
+                                FROM product AS P
+                                WHERE categoryid IN (SELECT C.categoryid FROM category AS C WHERE C.category='Console')
+                                AND P.platformid = {0}"""
+
+SELECT_PLATFORM_GAME_GENRES = """SELECT DISTINCT G.genre
+                                  FROM product AS P NATURAL JOIN genre AS G
+                                  WHERE categoryid IN (SELECT C.categoryid FROM category AS C WHERE C.category = 'Game') AND P.platformid = {0}"""
+
+SELECT_PLATFORM_ACCESORIES = """SELECT DISTINCT G.genre
+                                  FROM product AS P NATURAL JOIN genre AS G
+                                  WHERE categoryid IN (SELECT C.categoryid FROM category AS C WHERE C.category='Accessory')
+                                  AND P.platformid = {0}"""
+
+SELECT_PLATFORM_LATEST_PRODUCTS = """SELECT *
+                                      FROM product_details AS P
+                                      WHERE to_date(P.release,'YYYY-MM-DD') BETWEEN now()::DATE -  '30 days'::INTERVAL AND now()::DATE
+                                      AND P.platformid = {0}"""
+
+SELECT_PLATFORM_SPECIAL_PRODUCTS = """SELECT *
+                             FROM product_details
+                             WHERE INOFFER = true AND pid = {0}"""
