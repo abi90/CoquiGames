df3a52027d7c480626b96dcb6f6a29333142118c
abi90 <abisai.ramos@upr.edu>
2016-12-05 14:37:15 -0400

    Added UserOrdersController

---
+++ Client/CGWebApp/public/js/angular/controllers/UserOrdersController.js
@@ -0,0 +1,42 @@
+/**
+ * Created by abi on 12/5/16.
+ */
+/**
+ * Created by abi on 10/12/16.
+ */
+app.controller('UserOrdersController', ['$scope', '$location', 'authenticationSvc', 'auth', 'userwsapi', '$rootScope',
+    function ($scope, $location, authenticationSvc, auth, userwsapi, $rootScope){
+
+        // Scope Variables
+        $scope.userData = auth;
+        $scope.userOrder = [];
+
+        var logout = function () {
+
+            authenticationSvc.logout()
+                .then(function () {
+                    $scope.userData = null;
+                    $scope.userOrder = [];
+                    $location.path("/login.html");
+                }, function () {
+                    $scope.userData = null;
+                    $scope.userOrder = [];
+                    $location.path("/login.html");
+                });
+        };
+
+        var getUserOrder = function () {
+            userwsapi.getUserOrders(auth.uid, auth.uname, auth.token).then(
+                function (response) {
+                    $scope.userOrder = response.data;
+                },
+                function () {
+                    logout();
+                }
+            )
+        };
+
+        // Get User Data on startup
+        getUserOrder();
+
+    }]);
